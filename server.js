e8c1103298122934a6ed6c4b0cc34958f51b0ad1
Andrew Garcia <github@drewgarcia.dev>
2023-04-12 18:30:28 -0400

    pm2 and cluster examples

--- server.js
+++ server.js
@@ -10,12 +10,14 @@ function delay(duration) {
 }
 
 app.get('/', (req, res) => {
-  res.send('Performance example');
+  res.send(`Performance example: ${process.pid}`);
 });
 
 app.get('/timer', (req, res) => {
   delay(9000);
-  res.send('Ding ding ding!');
+  res.send(`Ding ding ding! ${process.pid}`);
 });
 
+console.log('Running server.js');
+console.log('Worker process started.');
 app.listen(3000);
